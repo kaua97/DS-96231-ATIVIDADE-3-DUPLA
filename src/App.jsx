ca7e2c906adb048cb970b236b40639ed7a1b1049
Denner <denner.sales@ba.estudante.senai.br>
2026-06-10 20:30:02 -0300

    mantos atualizado 80%

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import Header from './Componentes/Header';
 import Footer from './Componentes/Footer';
 import Home from './pages/Home';
 import Historia from './pages/Historia';
+import Mantos from './pages/Mantos';
 
 export default function App() {
   return (
     <Router>
       <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f3f4f6' }}>
         <Header />
-        <Home/>
-        <Historia/>
+      
         
         <main style={{ flex: 1, padding: '2rem' }}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/historia" element={<Historia />} />
           </Routes>
+          <Mantos/>
+          <Historia/>
+          
         </main>
 
         <Footer />
