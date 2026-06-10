2cb310dc7a46fd1ad787e72d5d13b66d486aefe7
Denner <denner.sales@ba.estudante.senai.br>
2026-06-10 19:45:10 -0300

    home melhorado visualmente

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,8 @@
     import './style.css';
-    
 
     export default function Home() {
     return (
         <div className="vitoria-theme">
-
         <main className="hero-container">
             <div className="hero-content">
             <span className="badge">Sou Mais Vitória</span>
@@ -12,13 +10,10 @@
             <p>Sinta a energia da maior torcida do Norte-Nordeste. O Barradão te espera.</p>
             <button className="btn-primary">Seja Sócio LEÃO</button>
             </div>
-            <div className="hero-image-slot">
-            <img src="/src/assets/barradao.jpg" alt="Estádio Manoel Barradas" />
-            <div className="floating-badge">
-                <img src=" " alt="Hexa Campeão" />
 
-
-            </div>
+            {/* Lado direito contendo APENAS o escudo isolado */}
+            <div className="escudo-container">
+            <img src="/Esporte_Clube_Vitória_(2024).svg.png" alt="Escudo Esporte Clube Vitória" />
             </div>
         </main>
         </div>
