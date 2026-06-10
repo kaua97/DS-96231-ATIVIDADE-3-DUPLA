c97dc1228ef214607696d672dd7cb4d6a3e4434f
kaua97 <kaua.v.santos@ba.estudante.senai.br>
2026-06-10 20:47:20 -0300

    KAUA

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,12 @@ import Header from './Componentes/Header';
 import Footer from './Componentes/Footer';
 import Home from './pages/Home';
 import Historia from './pages/Historia';
-<<<<<<< HEAD
-import Mantos from './pages/Mantos';
-=======
 import Mantos from './pages/Mantos'; // 1. Importando a página de Mantos que faltava
->>>>>>> f1051282f4aa8b50cd0a9926c110f628c841c014
 
 export default function App() {
   return (
     <Router>
       <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f3f4f6' }}>
-<<<<<<< HEAD
-        <Header />
-      
-=======
->>>>>>> f1051282f4aa8b50cd0a9926c110f628c841c014
         
         {/* O Header fica aqui fixo. Ele carrega os links que mudam a URL */}
         <Header />
@@ -34,9 +25,6 @@ export default function App() {
             {/* Quando a URL for "/mantos", renderiza apenas os Mantos */}
             <Route path="/mantos" element={<Mantos />} />
           </Routes>
-          <Mantos/>
-          <Historia/>
-          
         </main>
 
         {/* O Footer fica aqui fixo no final de todas as páginas */}
