b44a9b092d2aecfaa9c52ccb8ed5f6847198a06c
Denner <denner.sales@ba.estudante.senai.br>
2026-06-10 21:07:36 -0300

    mantos com imagens

--- src/pages/Mantos/index.jsx
+++ src/pages/Mantos/index.jsx
@@ -19,45 +19,46 @@
             </div>
 
             <div className="mantos-grid">
-            {/* Card 1 */}
             <div className="manto-card">
-                <div className="manto-img-wrapper">
-                <div className="manto-placeholder rubro-negro"></div>
-                <span className="manto-tag">Titular</span>
-                </div>
-                <div className="manto-info">
-                <h3>Manto Home 2026</h3>
-                <p>As tradicionais faixas horizontais carregadas de raça e paixão.</p>
-                <div className="price">R$ 349,90</div>
-                </div>
+            <div className="manto-img-wrapper">
+            <img src="/camisaVP1.jpg" className="public/camisaVP1.jpg" alt="Manto Home 2026" />
+            <span className="manto-tag">Titular</span>
+            </div>
+            <div className="manto-info">
+            <h3>Manto Rubro-Negro </h3>
+            <p>As tradicionais faixas vermelho e preto carregadas de raça e paixão.</p>
+            <div className="price">R$ 349,90</div>
+            </div>
             </div>
 
             {/* Card 2 */}
             <div className="manto-card">
-                <div className="manto-img-wrapper">
-                <div className="manto-placeholder branco-vitoria"></div>
-                <span className="manto-tag">Reserva</span>
-                </div>
-                <div className="manto-info">
-                <h3>Manto Away 2026</h3>
-                <p>A elegância do branco com detalhes marcantes em vermelho e preto.</p>
-                <div className="price">R$ 349,90</div>
-                </div>
+            <div className="manto-img-wrapper">
+              {/* COLOQUE A SUA IMAGEM 2 AQUI DENTRO DA PASTA public COM O NOME: camisa-reserva.jpg */}
+            <img src="/camisaBranca.jpg" className="public/camisaBranca.jpg" alt="Manto Away 2026" />
+            <span className="manto-tag">Reserva</span>
+            </div>
+            <div className="manto-info">
+            <h3>Manto Branco</h3>
+            <p>A elegância do branco com detalhes marcantes em vermelho e preto.</p>
+            <div className="price">R$ 349,90</div>
+            </div>
             </div>
 
             {/* Card 3 */}
             <div className="manto-card">
-                <div className="manto-img-wrapper">
-                <div className="manto-placeholder dourado-vitoria"></div>
-                <span className="manto-tag Especial">Especial</span>
-                </div>
-                <div className="manto-info">
-                <h3>Manto Consciência</h3>
-                <p>Edição especial dourada celebrando o orgulho e as raízes da Bahia.</p>
-                <div className="price">R$ 359,90</div>
-                </div>
+            <div className="manto-img-wrapper">
+              {/* COLOQUE A SUA IMAGEM 3 AQUI DENTRO DA PASTA public COM O NOME: camisa-especial.jpg */}
+            <img src="/camisaPreta.jpg" className="public/camisaPreta.jpg" alt="Manto Consciência" />
+            <span className="manto-tag Especial">Especial</span>
+            </div>
+            <div className="manto-info">
+            <h3>Manto Preto</h3>
+            <p>Edição especial o orgulho e as raízes da Bahia.</p>
+            <div className="price">R$ 359,90</div>
             </div>
             </div>
+        </div>
         </section>
         </div>
     );
